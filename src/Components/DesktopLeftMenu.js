834193e3fcc578b68212c002f35508b534ce5674
mathieu <mathieu.delepine@mmibordeaux.com>
2020-11-09 18:15:23 +0100

    proto2 leftmenu + _init.scss h1,h2,h3

--- src/Components/DesktopLeftMenu.js
+++ src/Components/DesktopLeftMenu.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DesktopLeftMenuItems from '../Components/DesktopLeftMenuItems.js';
-//import PropTypes from 'prop-types';
+//STYLE';
 import { styleDesktopLM } from './ComponentStyle/DesktopLeftMenu.module.scss';
 
 const DesktopLeftMenu = (props) => {
@@ -10,7 +10,7 @@ const DesktopLeftMenu = (props) => {
 				<h1>HOME</h1>
                 <h3>Bibliothèque</h3>
                 <DesktopLeftMenuItems/>
-                <h3>Playlist</h3>
+                <h3>Playlists</h3>
                 <h2>Rap US 2020</h2>
 			</div>
 	);
--- src/Components/DesktopLeftMenuItems.js
+++ src/Components/DesktopLeftMenuItems.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
+//STYLE
 import { styleDesktopLMI } from './ComponentStyle/DesktopLeftMenuItems.module.scss';
 
 const DesktopLeftMenuItems = (props) => {
