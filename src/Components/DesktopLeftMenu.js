eac5fb91952e7a361bbae0e77ac1d47cff196c90
mathieu <mathieu.delepine@mmibordeaux.com>
2020-11-10 11:58:44 +0100

    _init.scss UPDATE 2

--- src/Components/DesktopLeftMenu.js
+++ src/Components/DesktopLeftMenu.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import DesktopLeftMenuItems from '../Components/DesktopLeftMenuItems.js';
 //STYLE';
 import { styleDesktopLM } from './ComponentStyle/DesktopLeftMenu.module.scss';
 
@@ -9,7 +8,6 @@ const DesktopLeftMenu = (props) => {
 			<div className={ styleDesktopLM }>
 				<h1>HOME</h1>
                 <h3>Bibliothèque</h3>
-                <DesktopLeftMenuItems/>
                 <h3>Playlists</h3>
                 <h2>Rap US 2020</h2>
 			</div>
