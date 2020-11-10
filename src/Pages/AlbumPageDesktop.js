57bdbafad356eb9ea689030d602c2e219eaca224
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-10 13:00:34 +0100

    more album page

--- src/Pages/AlbumPageDesktop.js
+++ src/Pages/AlbumPageDesktop.js
@@ -18,7 +18,16 @@ const AlbumPageDesktop = (props) => {
 	return (
 			<div className={ styles.albumPage }>
 				<img src=""/>
-				<div className= { styles.headerMenu } >
+				<div>
+				 <h2 className={ styles.title }>Mania</h2>
+				 <h3 className={ styles.title2}>Fall out Boy</h3>
+				 <div className= { styles.genreAndDate }>
+				 	<h3 className={ [styles.corps ].join(' ') }>Alternative</h3>
+				 	<span> - </span>
+				 	<h3 className={ [styles.corps ].join(' ') }>2018</h3>
+				 </div>
+				</div>
+				<div className= { [styles.title3, styles.headerMenu].join(' ') } >
 					<span>Morceau</span>
 					<span>Artiste</span>
 					<span>Durée</span>
