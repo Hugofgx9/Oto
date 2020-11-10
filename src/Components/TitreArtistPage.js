2313d80d7e8fa576f300cdcea61d55535b2f542b
Nicolas Maitre <nicolasmaitre33@yahoo.fr>
2020-11-10 12:06:04 +0100

    update titreartistepage.js

--- src/Components/TitreArtistPage.js
+++ src/Components/TitreArtistPage.js
@@ -4,11 +4,9 @@ import React from 'react';
 const TitreArtistPage = (props) => {
 	//const { color } = props;
 	return (
-<<<<<<< HEAD:src/Components/TitreArtistPage.js
-			<div >
-=======
+
 			<div>
->>>>>>> eac5fb91952e7a361bbae0e77ac1d47cff196c90:src/Components/Titre.js
+
 				<p>
 					illustrationAlbum
 					trackName
