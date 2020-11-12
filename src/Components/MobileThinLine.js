2dbdee52303cffa9d23c496d71e31dbd4961245f
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-12 14:33:37 +0100

    mobile HomePage v1

---
+++ src/Components/MobileThinLine.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from '@components/ComponentStyle/MobileThinLine.module.scss';
+//import { Link } from 'react-router-dom';
+
+
+const MobileThinLine = (props) => {
+	const {className} = props;
+	return (
+		<hr className={ clsx(styles.thinLine, className) }/>
+	);
+}
+
+export default MobileThinLine; 
