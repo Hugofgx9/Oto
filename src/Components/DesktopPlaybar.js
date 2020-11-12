6603b39603f3a9b939829dc9a03ad57de2e266b8
mathieu <mathieu.delepine@mmibordeaux.com>
2020-11-12 12:38:41 +0100

    DesktopLeftMenu UPDATE Responsive

---
+++ src/Components/DesktopPlaybar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//styles
+import styles from './ComponentStyle/DesktopPlaybar.module.scss';
+
+const DesktopPlaybar = () => {
+	return (
+        <div className={ styles.DesktopPBGrid}>
+
+        </div>
+    );
+}
+export default DesktopPlaybar;
