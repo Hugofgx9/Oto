19d9afef8672d62bfd5eadb7f8eb485f50ac01b9
mathieu <mathieu-delepine@orange.fr>
2020-11-13 14:13:20 +0100

    Play/pause Mobile button

--- src/Components/mobile/MobilePlaybar.js
+++ src/Components/mobile/MobilePlaybar.js
@@ -1,8 +1,9 @@
-import React from 'react';
 import clsx from 'clsx';
+import React, { useState } from 'react';
 
 //SVG
 import playbutton from '@src/assets/img/play.svg';
+import pausebutton from '@src/assets/img/pause.svg';
 import homebutton from '@src/assets/img/home_white.svg';
 import queuebutton from '@src/assets/img/queue_white.svg';
 import searchbutton from '@src/assets/img/search_white.svg';
@@ -11,17 +12,19 @@ import searchbutton from '@src/assets/img/search_white.svg';
 import styles from '@components/mobile/style/MobilePlaybar.module.scss';
 
 const MobilePlaybar = () => {
+
+    const [isPlay, setPlay] = useState(false);
+
     return (
         <div className={ styles.MobilePlaybar}>
 
-           
-
             <div className={styles.leftPart}>
-                <img className={ styles.playButton} src={playbutton} alt="playbuton" />
-                <img className={ styles.cover} src = "https://cdn.hmv.com/r/w-1280/hmv/files/71/71d9cbc7-3bc0-44a5-9bb3-fa491bf9b694.jpg" alt = ""
-                />
+                <button onClick={() => setPlay(!isPlay)} className={ styles.playButton} >
+                    { isPlay ? <img src={pausebutton} alt="pause" /> : <img src={playbutton} alt="playbutton"/> }
+                </button>
+                {/* <img className={ styles.playButton} src={playbutton} alt="playbuton" /> */}
+                <img className={ styles.cover} src = "https://cdn.hmv.com/r/w-1280/hmv/files/71/71d9cbc7-3bc0-44a5-9bb3-fa491bf9b694.jpg" alt = ""/>
             </div>
-
             <div className = {styles.rightPart}>
                 <span className={ clsx(styles.titlePlayedTrack, styles.corps)}>
                     On The Turning Away
@@ -38,12 +41,6 @@ const MobilePlaybar = () => {
 
                 </div>
             </div>
-
-            
-
-            
-            
-
         </div>
     );
 }
