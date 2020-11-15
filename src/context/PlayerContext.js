2f1d5e3bc3dc13683d7cc15c21b370bc5ff829bd
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-11-15 13:24:23 +0100

    desktop LM link to spotify player

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -5,11 +5,18 @@ const context = createContext();
 const PlayerProvider = (props) => {
 	const { children } = props;
 	const [isPlay, setIsPlay] = useState(false);
+	const [currentlyPlaying, setCurrentlyPlaying] = useState({
+		track: {name: "", uri: ""},
+		artist: {name: "", uri: ""},
+		img: "https://images-na.ssl-images-amazon.com/images/I/710ZkhTLgJL._SY355_.jpg",
+	});
 
 	const contextValue = useMemo(() => ({
 		isPlay,
-		setIsPlay
-	}),[isPlay, setIsPlay])
+		setIsPlay,
+		currentlyPlaying,
+		setCurrentlyPlaying,
+	}),[isPlay, setIsPlay, currentlyPlaying, setCurrentlyPlaying])
 
 	return (
 		<context.Provider value={contextValue}>
